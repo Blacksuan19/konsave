9ddd808ec6f0c92621f4ff17629dfa0476dcd98e
Blacksuan19 <abubakaryagob@gmail.com>
2023-03-02 00:08:49 +0800

    parse: drop parse_{keywords, functions}
    part of Config class now
    
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- konsave/parse.py
+++ konsave/parse.py
@@ -39,51 +39,6 @@ def begins_with(grouped_regex, path) -> str:
     return occurence
 
 
-def parse_keywords(tokens_, token_symbol, parsed):
-    """Replaces keywords with values in conf.yaml. For example, it will replace, $HOME with
-    /home/username/
-
-    Args:
-        tokens_: the token dictionary
-        token_symbol: TOKEN_SYMBOL
-        parsed: the parsed conf.yaml file
-    """
-    for item in parsed:
-        for name in parsed[item]:
-            for key, value in tokens_["keywords"]["dict"].items():
-                word = token_symbol + key
-                location = parsed[item][name]["location"]
-                if word in location:
-                    parsed[item][name]["location"] = location.replace(word, value)
-
-
-def parse_functions(tokens_, token_symbol, parsed):
-    """Replaces functions with values in conf.yaml. For example, it will replace,
-    ${ENDS_WITH='text'} with a folder whose name ends with "text"
-
-    Args:
-        tokens_: the token dictionary
-        token_symbol: TOKEN_SYMBOL
-        parsed: the parsed conf.yaml file
-    """
-    functions = tokens_["functions"]
-    raw_regex = f"\\{token_symbol}{functions['raw_regex']}"
-    grouped_regex = f"\\{token_symbol}{functions['grouped_regex']}"
-
-    for item in parsed:
-        for name in parsed[item]:
-            location = parsed[item][name]["location"]
-            occurences = re.findall(raw_regex, location)
-            if not occurences:
-                continue
-            for occurence in occurences:
-                func = re.search(grouped_regex, occurence).group(1)
-                if func in functions["dict"]:
-                    parsed[item][name]["location"] = functions["dict"][func](
-                        grouped_regex, location
-                    )
-
-
 TOKEN_SYMBOL = "$"
 tokens = {
     "keywords": {
